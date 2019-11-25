1c6762b664388d8c3b81c2cc0c1ae5c76e3b2170
Garrick Aden-Buie <garrick.aden-buie@moffitt.org>
2019-11-25 18:55:33 -0500

    Add main package doc

---
+++ R/xaringanExtra-package.R
@@ -0,0 +1,20 @@
+#' xaringanExtra: Extensions for xaringan
+#'
+#' `xaringanExtra` is a playground of enhancements and addins for
+#' \pkg{xaringan} slides.
+#'
+#' - Add an overview of your presentation with [use_tile_view()]
+#' - Announce slide changes with a subtle tone: [use_slide_tone()]
+#' - Animate slide transitions with [use_animate_css()]
+#' - Use the Tachyons CSS utility toolkit: [use_tachyons()]
+#' - Create fancy poster-style text blocks with [text_poster()]
+#' - Fit your slides to fill the browser window: [use_fit_screen()]
+#'
+#' @keywords internal
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
+NULL
