bf70e54b3a3041cd0dbb80ed156fc95b8da31303
Garrick Aden-Buie <garrick.aden-buie@moffitt.org>
2019-11-25 17:16:48 -0500

    Add fit-screen with use_fit_screen()

---
+++ R/fit-screen.R
@@ -0,0 +1,47 @@
+#' Fit Slides to the Screen
+#'
+#' This extension resizes the slides to match the browser window height and
+#' width. In other words, the slides are maximized to match the screen size.
+#' The primary use case for this extension is for when you want to show your
+#' slides in split screen, for example when demonstrating code in RStudio or
+#' another window. To enable fit-to-screen, press **Alt**/**Option** + **F** during the
+#' slideshow. To disable, reload the slides.
+#'
+#' @return An [htmltools::tagList] with the fit-to-screen dependency, or an
+#'   [htmltools::htmlDependency].
+#'
+#' @section Usage: To enable fit-to-screen, add the following code chunk to your
+#'   slides:
+#'
+#'   ````markdown
+#'   ```{r xaringan-fit-screen, echo=FALSE}
+#'   xaringanExtra::use_fit_screen()
+#'   ```
+#'   ````
+#'
+#'   And then press **Alt**/**Option** + **F** at any point during your slide show to
+#'   enable the extension.
+#' @name fit_screen
+NULL
+
+#' @describeIn fit_screen Use the fit-to-screen extension in your xaringan slides.
+#' @export
+use_fit_screen <- function() {
+	htmltools::tagList(
+		html_dependency_fit_screen()
+	)
+}
+
+#' @describeIn fit_screen Returns an [htmltools::htmlDependency] with the fit
+#'   screen dependencies. Most users will want to use `use_fit_screen()`.
+#' @export
+html_dependency_fit_screen <- function() {
+	htmltools::htmlDependency(
+		name = "xaringanExtra_fit-screen",
+		version = utils::packageVersion("xaringanExtra"),
+		package = "xaringanExtra",
+		src = "fit-screen",
+		script = "fit-screen.js",
+		all_files = FALSE
+	)
+}
--- README.md
+++ README.md
@@ -17,6 +17,7 @@
 - Animate slide transitions with [animate.css](#-animatecss)
 - Use the [Tachyons CSS utility toolkit](#-tachyons)
 - Create fancy [poster-style text blocks](#-text-poster)
+- Fit your slides to [fill the browser window](#-fit-to-screen)
 
 ## Installation
 
@@ -200,5 +201,34 @@ class: center middle
 .text-poster {
 	font-family: 'Merriweather', serif;
 }
+```
+````
+
+## &#x1F4D0; Fit to Screen
+
+#### &#x1F4FA; [Fit to Screen Demo](https://gadenbuie.github.io/xaringanExtra/fit-screen)
+
+xaringan/remark slides scale at a consistent ratio when the browser window is resized.
+In other words, if the slide ratio is `4:3`, 
+then remark scales the slides and positions them in the browser window 
+so that they maintain the aspect ratio.
+
+In certain situations, 
+like when showing slides in split screen next to another window like RStudio,
+this causes the slides to become rather small.
+Alternatively,
+it's a great way to be able to adapt your slides 
+to the aspect ratio of the projector or television screen
+when you don't know the ratio ahead of time.
+
+This extension adds a short cut key — <kbd>Alt</kbd>/<kbd>Option</kbd>+ <kbd>F</kbd> —
+that fits the slides to the screen and ignores the slide ratio.
+(Currently, it only turns on; reload your slides to return to normal.)
+
+![](man/figures/fit-screen.gif)
+
+````markdown
+```{r xaringan-fit-screen, echo=FALSE}
+xaringanExtra::use_fit_screen()
 ```
 ````
