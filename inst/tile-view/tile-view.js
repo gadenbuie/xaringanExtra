40e8af91f485dea62320e8c314b64361f80eacb1
Garrick Aden-Buie <gadenbuie@mail.usf.edu>
2020-01-09 11:09:41 -0500

    Block browser zoom actions in tile view
    Prevents an issue where rescaling in tile view returns to an empty screen until the window is resized or the zoom is changed. This is because the slide rescaling in remark is broken by slides not being visible but there's no external API to redraw the slides.

--- inst/tile-view/tile-view.js
+++ inst/tile-view/tile-view.js
@@ -44,75 +44,7 @@
       el.style.display = el.style.display === 'none' ? '' : 'none';
     };
 
-    const createTileView = ({ minSize = 250, title = document.title } = {}) => {
-      // Tile view header
-      const h1 = document.createElement('h1');
-      h1.className = 'remark__tile-view__header';
-      h1.innerHTML = title;
-
-      tileView.appendChild(h1);
-      const tiles = document.createElement('div');
-      tiles.className = 'remark__tile-view__tiles';
-      tileView.appendChild(tiles);
-
-      // Clone slideshow
-      const slidesArea = remarkSlideShow.cloneNode(true);
-
-      // Calculate slide scale and tile container size
-      const slideScaler = slidesArea.querySelector('.remark-slide-scaler');
-      const slideWidth = parseFloat(slideScaler.style.width.replace('px', ''));
-      const slideHeight = parseFloat(
-        slideScaler.style.height.replace('px', '')
-      );
-      const scale = minSize / Math.min(slideWidth, slideHeight);
-      const tileWidth = Math.round(slideWidth * scale);
-      const tileHeight = Math.round(slideHeight * scale);
-
-      tiles.style.gridTemplateColumns = `repeat(auto-fill, minmax(${tileWidth +
-        10}px, 1fr))`;
-
-      const slides = slidesArea.querySelectorAll('.remark-slide-container');
-
-      slides.forEach((slide, slideIndex) => {
-        let tile = document.createElement('template');
-        tile.innerHTML = `<div class="remark__tile-view__tile">
-            <div class="remark__tile-view__slide-container">
-            </div></div>`;
-        tile = tile.content.firstChild;
-
-        const tileContainer = tile.querySelector(
-          '.remark__tile-view__slide-container'
-        );
-        tileContainer.style.width = `${tileWidth}px`;
-        tileContainer.style.height = `${tileHeight}px`;
-
-        const thisSlideScaler = slide.querySelector('.remark-slide-scaler');
-        thisSlideScaler.style.top = '0px';
-        thisSlideScaler.style.left = '0px';
-        thisSlideScaler.style.transform = `scale(${scale})`;
-        thisSlideScaler.parentElement.classList.add('remark-visible');
-
-        slide.addEventListener('click', () => {
-          toggleElement(tileView);
-          toggleElement(remarkSlideShow);
-          slideshow.gotoSlide(slideIndex + 1);
-        });
-
-        tileContainer.appendChild(slide);
-        tiles.appendChild(tile);
-      });
-
-      document.body.appendChild(tileView);
-    };
-
-    const tileVars = {};
-    const blockEvent = ev => ev.stopPropagation();
-
-    document.addEventListener('keyup', ev => {
-      if (ev.keyCode !== launchKey) {
-        return;
-      }
-
+    const toggleTileView = function() {
       toggleElement(tileView);
       toggleElement(remarkSlideShow);
 
@@ -123,6 +55,8 @@
         // remove scroll/mousewheel event blocking
         tileView.removeEventListener('mousewheel', blockEvent);
         tileView.removeEventListener('DOMMouseScroll', blockEvent);
+        console.log('removing blockScaling')
+        document.removeEventListener('keydown', blockScaling);
       } else {
         // store current slide index prior to launching tile-view
         tileVars.currentSlideIdx = slideshow.getCurrentSlideIndex();
@@ -147,6 +81,88 @@
         // block remarkjs from handling scroll events
         tileView.addEventListener('mousewheel', blockEvent);
         tileView.addEventListener('DOMMouseScroll', blockEvent);
+        console.log('adding blockScaling')
+        document.addEventListener('keydown', blockScaling);
+      }
+    }
+
+    const createTileView = ({ minSize = 250, title = document.title } = {}) => {
+      // Tile view header
+      const h1 = document.createElement('h1');
+      h1.className = 'remark__tile-view__header';
+      h1.innerHTML = title;
+
+      tileView.appendChild(h1);
+      const tiles = document.createElement('div');
+      tiles.className = 'remark__tile-view__tiles';
+      tileView.appendChild(tiles);
+
+      // Clone slideshow
+      const slidesArea = remarkSlideShow.cloneNode(true);
+
+      // Calculate slide scale and tile container size
+      const slideScaler = slidesArea.querySelector('.remark-slide-scaler');
+      const slideWidth = parseFloat(slideScaler.style.width.replace('px', ''));
+      const slideHeight = parseFloat(
+        slideScaler.style.height.replace('px', '')
+      );
+      const scale = minSize / Math.min(slideWidth, slideHeight);
+      let tileWidth = Math.round(slideWidth * scale);
+      let tileHeight = Math.round(slideHeight * scale);
+
+      // convert tileWidth/Height to em relative to base 18px (set in CSS)
+      tileWidth = tileWidth / 18
+      tileHeight = tileHeight / 18
+
+      tiles.style.gridTemplateColumns = `repeat(auto-fill, minmax(${tileWidth}em, 1fr))`;
+
+      const slides = slidesArea.querySelectorAll('.remark-slide-container');
+
+      slides.forEach((slide, slideIndex) => {
+        let tile = document.createElement('template');
+        tile.innerHTML = `<div class="remark__tile-view__tile">
+            <div class="remark__tile-view__slide-container">
+            </div></div>`;
+        tile = tile.content.firstChild;
+
+        const tileContainer = tile.querySelector(
+          '.remark__tile-view__slide-container'
+        );
+        tileContainer.style.width = `${tileWidth}em`;
+        tileContainer.style.height = `${tileHeight}em`;
+
+        const thisSlideScaler = slide.querySelector('.remark-slide-scaler');
+        thisSlideScaler.style.top = '0px';
+        thisSlideScaler.style.left = '0px';
+        thisSlideScaler.style.transform = `scale(${scale})`;
+        thisSlideScaler.parentElement.classList.add('remark-visible');
+
+        slide.addEventListener('click', () => {
+          tileVars.currentSlideIdx = slideIndex
+          toggleTileView()
+        });
+
+        tileContainer.appendChild(slide);
+        tiles.appendChild(tile);
+      });
+
+      document.body.appendChild(tileView);
+    };
+
+    const tileVars = {};
+    const blockEvent = ev => ev.stopPropagation();
+    const blockScaling = function(ev) {
+      if (ev.controlKey || ev.metaKey) {
+         if (ev.key === '=' || ev.key === '-') {
+           ev.preventDefault()
+           console.log('window scaling is not allowed inside the tile overview')
+         }
+      }
+    }
+
+    document.addEventListener('keyup', ev => {
+      if (ev.keyCode === launchKey) {
+        toggleTileView()
       }
     });
 
