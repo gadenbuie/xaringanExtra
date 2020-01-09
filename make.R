57764be31d8c14c79d2460897cc6fdf1ade51477
Garrick Aden-Buie <gadenbuie@mail.usf.edu>
2020-01-09 11:31:31 -0500

    update docs

--- make.R
+++ make.R
@@ -3,12 +3,15 @@ devtools::install(dependencies = FALSE)
 
 docs <- file.path(
 	"docs",
-	c("slide-tone", "tile-view", "animate-css", "tachyons", "text-poster")
+	c("slide-tone", "tile-view", "animate-css", "tachyons", "text-poster", "panelset")
 )
 
 for (doc in docs) {
-	if (dir.exists(file.path(doc, "libs"))) {
-		unlink(file.path(doc, "libs"), recursive = TRUE)
+	if (dir.exists(lib_path <- file.path(doc, "libs"))) {
+		unlink(lib_path, recursive = TRUE)
+	}
+	if (dir.exists(files_path <- file.path(doc, "index_files"))) {
+		unlink(files_path, recursive = TRUE)
 	}
 	rmarkdown::render(file.path(doc, "index.Rmd"))
 }
