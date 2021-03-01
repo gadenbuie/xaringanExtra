688b896f75ca6d81bdc3a8f0778792cd1a706921
Matthew T. Warkentin <matt.warkentin@mail.utoronto.ca>
2021-03-01 15:56:57 -0500

    Canvas path objects now scale with slide; added SVGs as class objects

--- inst/scribble/xaringanExtra-scribble.js
+++ inst/scribble/xaringanExtra-scribble.js
@@ -30,7 +30,7 @@ class Scribble {
     this.eraser_impl = this.eraser_impl.bind(this)
 
     // Toolbox objects
-    this.launchKey = 83 // "S" used to toggle toolbox viz
+    this.launchKey = 83 // "S" used to toggle toolbox/state
     this.toolBox
     this.btnWrapper
     this.colWrapper
@@ -169,39 +169,23 @@ class Scribble {
       div.style.top = scalerSize.top + 'px'
     })
 
+    let scaleRatio = parseFloat(
+      this.getVisibleSlideScaleRatio().match('\\d+.\\d+'),
+    )
+
     // Resize canvas itself
     this.fabrics.forEach((fab) => {
       fab.setWidth(scalerSize.width)
       fab.setHeight(scalerSize.height)
+      fab.setZoom(scaleRatio)
     })
-  }
-
-  fetchIcon(name) {
-    var svg
-    switch (name) {
-      case 'draw':
-        svg =
-          '<svg viewBox="0 0 576 512"><path d="M402.3 344.9l32-32c5-5 13.7-1.5 13.7 5.7V464c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V112c0-26.5 21.5-48 48-48h273.5c7.1 0 10.7 8.6 5.7 13.7l-32 32c-1.5 1.5-3.5 2.3-5.7 2.3H48v352h352V350.5c0-2.1.8-4.1 2.3-5.6zm156.6-201.8L296.3 405.7l-90.4 10c-26.2 2.9-48.5-19.2-45.6-45.6l10-90.4L432.9 17.1c22.9-22.9 59.9-22.9 82.7 0l43.2 43.2c22.9 22.9 22.9 60 .1 82.8zM460.1 174L402 115.9 216.2 301.8l-7.3 65.3 65.3-7.3L460.1 174zm64.8-79.7l-43.2-43.2c-4.1-4.1-10.8-4.1-14.8 0L436 82l58.1 58.1 30.9-30.9c4-4.2 4-10.8-.1-14.9z"/></svg>'
-        break
-
-      case 'eraser':
-        svg =
-          '<svg viewBox="0 0 512 512"><path d="M497.941 273.941c18.745-18.745 18.745-49.137 0-67.882l-160-160c-18.745-18.745-49.136-18.746-67.883 0l-256 256c-18.745 18.745-18.745 49.137 0 67.882l96 96A48.004 48.004 0 0 0 144 480h356c6.627 0 12-5.373 12-12v-40c0-6.627-5.373-12-12-12H355.883l142.058-142.059zm-302.627-62.627l137.373 137.373L265.373 416H150.628l-80-80 124.686-124.686z"/></svg>'
-        break
-
-      case 'trash':
-        svg =
-          '<svg viewBox="0 0 448 512"><path d="M268 416h24a12 12 0 0 0 12-12V188a12 12 0 0 0-12-12h-24a12 12 0 0 0-12 12v216a12 12 0 0 0 12 12zM432 80h-82.41l-34-56.7A48 48 0 0 0 274.41 0H173.59a48 48 0 0 0-41.16 23.3L98.41 80H16A16 16 0 0 0 0 96v16a16 16 0 0 0 16 16h16v336a48 48 0 0 0 48 48h288a48 48 0 0 0 48-48V128h16a16 16 0 0 0 16-16V96a16 16 0 0 0-16-16zM171.84 50.91A6 6 0 0 1 177 48h94a6 6 0 0 1 5.15 2.91L293.61 80H154.39zM368 464H80V128h288zm-212-48h24a12 12 0 0 0 12-12V188a12 12 0 0 0-12-12h-24a12 12 0 0 0-12 12v216a12 12 0 0 0 12 12z"/></svg>'
-        break
-    }
-    return svg
   }
 
   createButton(id, name) {
     const btn = document.createElement('button')
     btn.classList.add('xe-scribble', 'tool-btn')
     btn.id = id
-    btn.innerHTML = this.fetchIcon(name)
+    btn.innerHTML = this.svgs[name]
     return btn
   }
 
@@ -252,8 +236,6 @@ class Scribble {
 
   addToolboxToSlide() {
     const canvasDiv = this.getVisibleSlideOuterContainer()
-    this.currColor = this.penColors[0]
-    this.colorPicker.value = this.penColors[0]
     canvasDiv.appendChild(this.toolBox)
     if (this.hideToolbox) {
       this.toolBox.classList.add('hidden')
@@ -265,6 +247,17 @@ class Scribble {
       if (ev.keyCode === this.launchKey) {
         this.toolBox.classList.toggle('hidden')
         this.hideToolbox = !this.hideToolbox
+
+        // Start draw mode when revealing toolbox
+        if (!this.hideToolbox & !this.drawMode) this.drawBtn.click()
+
+        if (this.hideToolbox) {
+          this.drawMode
+            ? this.drawBtn.click()
+            : this.eraseMode
+            ? this.eraseBtn.click()
+            : null
+        }
       }
     })
   }
@@ -293,7 +286,7 @@ class Scribble {
     this.mouseDown = false
   }
 
-  getCanvasPaths(id) {
+  getCanvasPaths() {
     return this.currFabric.getObjects('path')
   }
 
@@ -487,4 +480,13 @@ class Scribble {
     this.eraserCursor.style.top = pY - this.tolerance + 'px'
     this.eraserCursor.style.left = pX - this.tolerance + 'px'
   }
+}
+
+Scribble.prototype.svgs = {
+  draw:
+    '<svg viewBox="0 0 576 512"><path d="M402.3 344.9l32-32c5-5 13.7-1.5 13.7 5.7V464c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V112c0-26.5 21.5-48 48-48h273.5c7.1 0 10.7 8.6 5.7 13.7l-32 32c-1.5 1.5-3.5 2.3-5.7 2.3H48v352h352V350.5c0-2.1.8-4.1 2.3-5.6zm156.6-201.8L296.3 405.7l-90.4 10c-26.2 2.9-48.5-19.2-45.6-45.6l10-90.4L432.9 17.1c22.9-22.9 59.9-22.9 82.7 0l43.2 43.2c22.9 22.9 22.9 60 .1 82.8zM460.1 174L402 115.9 216.2 301.8l-7.3 65.3 65.3-7.3L460.1 174zm64.8-79.7l-43.2-43.2c-4.1-4.1-10.8-4.1-14.8 0L436 82l58.1 58.1 30.9-30.9c4-4.2 4-10.8-.1-14.9z"/></svg>',
+  eraser:
+    '<svg viewBox="0 0 512 512"><path d="M497.941 273.941c18.745-18.745 18.745-49.137 0-67.882l-160-160c-18.745-18.745-49.136-18.746-67.883 0l-256 256c-18.745 18.745-18.745 49.137 0 67.882l96 96A48.004 48.004 0 0 0 144 480h356c6.627 0 12-5.373 12-12v-40c0-6.627-5.373-12-12-12H355.883l142.058-142.059zm-302.627-62.627l137.373 137.373L265.373 416H150.628l-80-80 124.686-124.686z"/></svg>',
+  trash:
+    '<svg viewBox="0 0 448 512"><path d="M268 416h24a12 12 0 0 0 12-12V188a12 12 0 0 0-12-12h-24a12 12 0 0 0-12 12v216a12 12 0 0 0 12 12zM432 80h-82.41l-34-56.7A48 48 0 0 0 274.41 0H173.59a48 48 0 0 0-41.16 23.3L98.41 80H16A16 16 0 0 0 0 96v16a16 16 0 0 0 16 16h16v336a48 48 0 0 0 48 48h288a48 48 0 0 0 48-48V128h16a16 16 0 0 0 16-16V96a16 16 0 0 0-16-16zM171.84 50.91A6 6 0 0 1 177 48h94a6 6 0 0 1 5.15 2.91L293.61 80H154.39zM368 464H80V128h288zm-212-48h24a12 12 0 0 0 12-12V188a12 12 0 0 0-12-12h-24a12 12 0 0 0-12 12v216a12 12 0 0 0 12 12z"/></svg>',
 }
