9b67f8ca05da626b23a476bbea2406ad24387e17
Garrick Aden-Buie <garrick@adenbuie.com>
2021-03-10 10:38:45 -0500

    basic freezeframe

---
+++ R/freezeframe.R
@@ -0,0 +1,21 @@
+use_freezeframe <- function() {
+  htmltools::tagList(
+    html_dependency_freezeframe()
+  )
+}
+
+html_dependency_freezeframe <- function() {
+  htmltools::htmlDependency(
+    name = "freezeframe",
+    version = "5.0.2",
+    package = "xaringanExtra",
+    src = "jslib/freezeframe",
+    script = "freezeframe.min.js",
+    head = paste0(
+      "<script>document.addEventListener('DOMContentLoaded', function() {
+        window.xeFreezeframe = new Freezeframe('.remark-slides-area')
+      })</script>"
+    ),
+    all_files = FALSE
+  )
+}
