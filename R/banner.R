366707a2d3af3befb0b2c1c7c3c5bb97c76ef7f3
Garrick Aden-Buie <garrick@adenbuie.com>
2022-06-05 07:57:55 -0400

    Add ... to `use_banner()` for `style_banner()` calls

--- R/banner.R
+++ R/banner.R
@@ -10,7 +10,8 @@
 #' use_banner(
 #'   bottom_left = "bit.ly/my-awesome-slides",
 #'   top_center = "My Presentation",
-#'   exclude = c("title-slide", "inverse")
+#'   exclude = c("title-slide", "inverse"),
+#'   style_banner(text_color = "grey")
 #' )
 #'
 #' @param bottom_left,top_left Text or HTML to place in the left column of the
@@ -22,8 +23,12 @@
 #' @param exclude A vector of slide classes where the banner should not be
 #'   applied. By default all slides are included, but you might want to exclude
 #'   the title and inverse slides with `exclude = c("title-slide", "inverse")`.
+#' @param ... Banner styles created with [style_banner()]. Technically,
+#'   additional arguments are added into the [htmltools::tagList()] with the
+#'   banner dependencies, but you'll only want to use [style_banner()] calls
+#'   here.
 #'
-#' @return An `htmltools::tagList()` with the banner dependencies, or an
+#' @return An [htmltools::tagList()] with the banner dependencies, or an
 #'   [htmltools::htmlDependency].
 #'
 #' @seealso [style_banner()]
@@ -35,7 +40,8 @@ use_banner <- function(
   top_left = NULL,
   top_center = NULL,
   top_right = NULL,
-  exclude = NULL
+  exclude = NULL,
+  ...
 ) {
   top <- init_banner(top_left, top_center, top_right, "top", exclude)
   bottom <- init_banner(bottom_left, bottom_center, bottom_right, "bottom", exclude)
@@ -43,6 +49,7 @@ use_banner <- function(
   htmltools::tagList(
     top,
     bottom,
+    ...,
     htmltools::htmlDependency(
       name = "xaringanExtra-banner",
       version = "0.0.1",
