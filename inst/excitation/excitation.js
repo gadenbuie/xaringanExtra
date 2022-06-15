dfeb03edf1405d28d014a182a08c38bd30e8ac31
Garrick Aden-Buie <garrick@adenbuie.com>
2022-06-15 10:02:30 -0400

    use helper functions to get tooltip and decode encoded html
    This method in usually unsafe(ish), but we're the ones creating the HTML, so it's all good

--- inst/excitation/excitation.js
+++ inst/excitation/excitation.js
@@ -4,16 +4,34 @@
  *  and Garrick Aden-Buie <garrick@adenbuie.com>
  */
 window.addEventListener('DOMContentLoaded', (event) => {
+  function htmlDecode (text) {
+    const elem = document.createElement('textarea');
+    elem.innerHTML = text;
+    return elem.value; // decoded
+  }
+
+  function getTooltipData (key) {
+    const tooltip_data = document.querySelector(`script[data-excitation-key=${key}]`)
+
+    return tooltip_data
+      ? JSON.parse(htmlDecode(tooltip_data.textContent))
+      : undefined
+  }
+
   let excite_spans = document.querySelectorAll('span.xe-excitation__tooltip')
+
   excite_spans.forEach((span) => {
     let key = span.getAttribute('data-excitation-key')
     let num_id = span.getAttribute('data-excitation-id')
-    let tooltip_data = document.querySelector(
-      `script[data-excitation-key=${key}]`
-    )
+
+    const tooltip_data = getTooltipData(key)
+    if (!tooltip_data) {
+      return
+    }
+
     span.innerHTML = num_id
     tippy(span, {
-      content: tooltip_data.textContent,
+      content: tooltip_data,
       allowHTML: true,
       hideOnClick: true,
       interactive: true,
